15c7f706e8f821008cc91e804620d8a4988ea6c8
erri120 <erri120@protonmail.com>
2020-05-14 20:55:51 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,45 @@
+﻿using NetScriptFramework.SkyrimSE;
+
+namespace CTDDeath
+{
+    public class Plugin : NetScriptFramework.Plugin
+    {
+        public override string Key => "erri120.crash-on-death";
+        public override string Name => "Crash on Death";
+        public override int Version => 1;
+
+        public override string Author => "erri120";
+        public override string Website => "https://github.com/erri120";
+
+        public override int RequiredFrameworkVersion => 9;
+        public override int RequiredLibraryVersion => 13;
+
+        private bool _inMainMenu;
+
+        protected override bool Initialize(bool loadedAny)
+        {
+            Events.OnMainMenu.Register(e =>
+            {
+                _inMainMenu = e.Entering;
+            });
+
+            Events.OnFrame.Register(e =>
+            {
+                if (Main.Instance == null)
+                    return;
+
+                if (_inMainMenu || Main.Instance.IsGamePaused)
+                    return;
+
+                var player = PlayerCharacter.Instance;
+                if (player == null)
+                    return;
+
+                if (player.IsDead)
+                    Main.Instance.QuitGame = true;
+            });
+
+            return true;
+        }
+    }
+}
